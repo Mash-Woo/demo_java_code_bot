23319c10ed96eb3876ef1091917f915604ea85df
Mạnh Nguyễn <manh.nguyenvu@ipas.com.vn>
2026-01-05 17:31:33 +0700

    feat: add print

--- src/main/java/io/github/delirius325/jmeter/backendlistener/elasticsearch/ElasticsearchBackendClient.java
+++ src/main/java/io/github/delirius325/jmeter/backendlistener/elasticsearch/ElasticsearchBackendClient.java
@@ -99,6 +99,7 @@ public class ElasticsearchBackendClient extends AbstractBackendListenerClient {
 
     @Override
     public void setupTest(BackendListenerContext context) throws Exception {
+        print("Kiểm tra code bot commit")
         try {
             this.filters = new HashSet<>();
             this.fields = new HashSet<>();
